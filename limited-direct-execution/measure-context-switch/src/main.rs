69e5e9940579debdfcdb03c168c6ada7c4d5e02b
Samuel Harvey-Collins <samuel.harvey-collins@conjure.co.uk>
2024-02-07 17:18:34 +0000

    measure-context-switch - Print gettimeofday

--- limited-direct-execution/measure-context-switch/src/main.rs
+++ limited-direct-execution/measure-context-switch/src/main.rs
@@ -1,14 +1,35 @@
 //Just print the result of the linux sys-call gettimeofday()
 
 use libc::{gettimeofday, timeval};
+use std::process::exit;
 
-fn main() {
+pub fn main() {
+    for _ in 0..100 {
+        match get_time_of_day() {
+            Ok(tv) => print_timeval(&tv),
+            Err(e) => {
+                eprintln!("{}", e);
+                exit(1);
+            }
+        }
+    }
+}
+
+fn print_timeval(tv: &timeval) {
+    println!("{}, {}", tv.tv_sec, tv.tv_usec);
+}
+
+fn get_time_of_day() -> Result<timeval, &'static str> {
     let mut tv = timeval {
         tv_sec: 0,
         tv_usec: 0,
     };
 
-    let time = unsafe { gettimeofday(&mut tv, std::ptr::null_mut()) };
+    let err = unsafe { gettimeofday(&mut tv, std::ptr::null_mut()) };
 
-    println!("{}", time);
+    if err != 0 {
+        return Err("gettimeofday failed");
+    }
+
+    Ok(tv)
 }
